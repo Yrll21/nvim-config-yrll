e553db6dc03ddcd8ac03c6263534d27ded4b9931
Ivan Yrll Labadia <yrll@Ivans-Air.lan>
2024-07-31 00:41:31 +0800

    Initial commit edit email

---
+++ init.lua
@@ -0,0 +1,5 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+-- Set transparency
+vim.cmd("highlight Normal guibg=NONE ctermbg=NONE")
+vim.cmd("highlight NonText guibg=NONE ctermbg=NONE")
