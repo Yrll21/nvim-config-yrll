f8306dec82a5004f399cac094134e1e4bf6ff40a
Yrll <101014492+Yrll21@users.noreply.github.com>
2024-08-14 08:26:44 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -3,3 +3,21 @@ require("config.lazy")
 -- Set transparency
 vim.cmd("highlight Normal guibg=NONE ctermbg=NONE")
 vim.cmd("highlight NonText guibg=NONE ctermbg=NONE")
+
+-- Configure NeoTree for transparency
+vim.cmd([[autocmd FileType neo-tree setlocal winhighlight=Normal:NormalNC]])
+
+-- Ensure transparency for normal and NeoTree highlight groups
+
+vim.cmd([[
+  highlight Normal guibg=NONE ctermbg=NONE
+  highlight EndOfBuffer guibg=NONE ctermbg=NONE
+  highlight NeoTreeNormal guibg=NONE ctermbg=NONE
+  highlight NeoTreeEndOfBuffer guibg=NONE ctermbg=NONE
+  highlight NeoTreeNormalNC guibg=NONE ctermbg=NONE
+]])
+
+vim.opt.termguicolors = true
+vim.cmd([[
+  highlight NormalNC guibg=NONE ctermbg=NONE
+]])
